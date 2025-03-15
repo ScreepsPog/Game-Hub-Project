4552b6c85728bd5ecf7b29c105c515237fa077bf
ScreepsPog <85161132+ScreepsPog@users.noreply.github.com>
2025-03-14 18:48:41 -0700

    Installing Chakra UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { Button, Grid } from "@chakra-ui/react"
+import { Button } from "@chakra-ui/react"
 
 function App() {
 
   return (
     <div>
-      <Grid></Grid>
+      <Button colorScheme="blue">Button</Button>
     </div>
   )
 }
