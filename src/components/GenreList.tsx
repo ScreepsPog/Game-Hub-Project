e60699a20020a7a5068078bb6128cd099eefa855
ScreepsPog <85161132+ScreepsPog@users.noreply.github.com>
2025-03-16 00:46:49 -0700

    Showing a Spinner (Replaced with Skeleton)

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,24 +1,40 @@
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
+import GenreListSkeleton from "./GenreListSkeleton";
 
 const GenreList = () => {
-  const { data } = useGenres();
+  const { data, isLoading, error } = useGenres();
+  const skeletons = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+  ];
+
+  if (error) return null;
+
   return (
-    <List>
-      {data.map((genre) => (
-        <ListItem key={genre.id} paddingY="5px">
-          <HStack>
-            <Image
-              boxSize="32px"
-              borderRadius={8}
-              src={getCroppedImageUrl(genre.image_background)}
-            />
-            <Text fontSize="lg">{genre.name}</Text>
-          </HStack>
-        </ListItem>
-      ))}
-    </List>
+    <>
+      {isLoading && (
+        <List>
+          {skeletons.map((skeleton) => (
+            <GenreListSkeleton key={skeleton} />
+          ))}
+        </List>
+      )}
+      <List>
+        {data.map((genre) => (
+          <ListItem key={genre.id} paddingY="5px">
+            <HStack>
+              <Image
+                boxSize="32px"
+                borderRadius={8}
+                src={getCroppedImageUrl(genre.image_background)}
+              />
+              <Text fontSize="lg">{genre.name}</Text>
+            </HStack>
+          </ListItem>
+        ))}
+      </List>
+    </>
   );
 };
 
---
+++ src/components/GenreListSkeleton.tsx
@@ -0,0 +1,11 @@
+import { ListItem, Skeleton } from '@chakra-ui/react'
+
+const GenreListSkeleton = () => {
+  return (
+    <ListItem paddingY="5px">
+        <Skeleton height="32px"/>
+    </ListItem>
+  )
+}
+
+export default GenreListSkeleton
