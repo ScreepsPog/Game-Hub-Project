3d80cf70c2392ea2251d514a25a38f54848b89fd
ScreepsPog <85161132+ScreepsPog@users.noreply.github.com>
2025-03-20 02:35:47 -0700

    Added Alt and Title to Logo Image

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -10,7 +10,7 @@ interface Props {
 const NavBar = ({ onSearch }: Props) => {
   return (
     <HStack padding="10px">
-      <Image src={logo} boxSize="60px" />
+      <Image src={logo} boxSize="60px" alt="Game Hub Logo" title="Game Hub Logo" />
       <SearchInput onSearch={onSearch} />
       <ColorModeSwitch />
     </HStack>
